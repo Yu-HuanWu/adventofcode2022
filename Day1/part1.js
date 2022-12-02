c8230c1fc1d9a2a87b14e2705dbad1c34ba9e059
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-12-02 15:35:17 -0800

    part 2 done

--- Day1/part1.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('/Users/yu-huanwu/Desktop/adventofcode2022/Day1/input', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
-});
