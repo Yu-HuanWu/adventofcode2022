c1d17e13c8e0bb4af487e8f04ce4fbf53fc3fe6a
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-12-03 00:58:33 -0800

    day 3 finished

--- Day03/day3.js
+++ Day03/day3.js
@@ -1,3 +1,12 @@
+function alphabetPoints(char){
+    let alphabets= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for (let i=0; i < alphabets.length; i++){
+        if (char === alphabets[i]) {
+            return i+1
+        }
+    }
+}
+
 const fs = require('fs');
 
 fs.readFile('Day03/input', 'utf8', (err, data) => {
@@ -8,21 +17,64 @@ fs.readFile('Day03/input', 'utf8', (err, data) => {
 
     // part 1
     let parsedData = []
+    // data = "mjpsHcssDzLTzMsz"
     data.split('\n').forEach(datum=> {
         let splitDatum = [datum.slice(0, datum.length / 2), datum.slice(datum.length/2, datum.length)];
         parsedData.push(splitDatum)
     })
     let repeated = []
-    parsedData.forEach(datum=> {
-        for (let i= 0; i< datum[0].length; i++) {
-            for (let j= 0; j< datum[1].length; j++) {
-                if (datum[0][i] === datum[1][j]) {
-                    repeated.push(datum[0][i])
-                    j += 1;
-                    i += 1;
+    for (let h= 0; h < parsedData.length; h++) {
+        let keepGoing = true;
+        for (let i = 0; i < parsedData[h][0].length; i++) {
+            for (let j = 0; j < parsedData[h][1].length; j++) {
+                if (!keepGoing) {
+                    break
+                }
+                if (parsedData[h][0][i] === parsedData[h][1][j]) {
+                    repeated.push(parsedData[h][0][i])
+                    keepGoing= false;
                 }
             }
         }
+    }
+    let points = 0
+    repeated.forEach(char=> {
+        points+= alphabetPoints(char)
     })
-    console.log(repeated)
+    console.log(points)
+
+    // part2
+    let triGroup = []
+    let finalArr = []
+    data.split('\n').forEach(datum=>{
+        triGroup.push(datum)
+        if (triGroup.length === 3) {
+            finalArr.push(triGroup);
+            triGroup = []
+        }
+    })
+    let repeatedV2 = []
+    for (let h = 0; h < finalArr.length; h++) {
+        let keepGoing = true;
+        for (let i = 0; i < finalArr[h][0].length; i++) {
+
+            for (let j = 0; j < finalArr[h][1].length; j++) {
+
+                for (let k = 0; k < finalArr[h][2].length; k++) {
+                    if (!keepGoing) {
+                        break
+                    }
+                    if (finalArr[h][0][i] === finalArr[h][1][j] && finalArr[h][1][j] === finalArr[h][2][k]) {
+                        repeatedV2.push(finalArr[h][0][i])
+                        keepGoing = false;
+                    }
+                }
+            }
+        }
+    }
+    let pointsV2 = 0
+    repeatedV2.forEach(char => {
+        pointsV2 += alphabetPoints(char)
+    })
+    console.log(pointsV2)
 });
