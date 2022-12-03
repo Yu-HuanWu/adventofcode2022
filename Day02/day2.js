4d65864ccede8bf90dd1f162f17c678c487ef3ce
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-12-02 16:15:23 -0800

    update

---
+++ Day02/day2.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+fs.readFile('/Users/yu-huanwu/Desktop/adventofcode2022/Day02/input', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    // part 1
+    
+});
