6c8ba759e02495b10176b02ed98fad00b6811110
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-12-10 10:26:57 -0800

    finished part 2

--- Day04/day4.js
+++ Day04/day4.js
@@ -24,4 +24,20 @@ fs.readFile('Day04/input', 'utf8', (err, data) => {
     })
 
     console.log(overlap)
+
+    // part 2
+
+    let overlap2 = 0;
+    parsedData.forEach((datum) => {
+        let first = datum[0].split("-");
+        let second = datum[1].split("-");
+        if ((parseInt(first[0]) <= parseInt(second[0]) && parseInt(first[1]) >= parseInt(second[1])) ||
+            (parseInt(first[0]) >= parseInt(second[0]) && parseInt(first[1]) <= parseInt(second[1])) ||
+            (parseInt(first[1]) >= parseInt(second[0]) && parseInt(first[1]) <= parseInt(second[1])) ||
+            (parseInt(first[0]) <= parseInt(second[1]) && parseInt(first[0]) >= parseInt(second[0]))
+        ) {
+            overlap2 += 1;
+        }
+    })
+    console.log(overlap2)
 })
