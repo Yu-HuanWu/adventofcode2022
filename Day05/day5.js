f032433cb33385fe4692028706cbffbb86b7eac0
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-12-15 08:48:31 -0800

    create crate

--- Day05/day5.js
+++ Day05/day5.js
@@ -1,3 +1,15 @@
+let crate = [
+    ['C', 'Z', 'N', 'B', 'M', 'W', 'Q', 'V'],
+    ['H', 'Z', 'R', 'W', 'C', 'B'],
+    ['F', 'Q', 'R', 'J'],
+    ['Z', 'S', 'W', 'H', 'F', 'N', 'M', 'T'],
+    ['G', 'F', 'W', 'L', 'N', 'Q', 'P'],
+    ['L', 'P', 'W'],
+    ['V', 'B', 'D', 'R', 'G', 'C', 'Q', 'J'],
+    ['Z', 'Q', 'N', 'B', 'W'],
+    ['H', 'L', 'F', 'C', 'G', 'T', 'J'],
+]
+
 const fs = require('fs');
 
 fs.readFile('Day05/input2', 'utf8', (err, data) => {
