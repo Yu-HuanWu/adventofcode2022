ee008b93feeb1d6dcbc02fa92e5737aecf335130
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-12-18 11:25:59 -0800

    finished part 2

--- Day05/day5.js
+++ Day05/day5.js
@@ -1,4 +1,4 @@
-let crate = [
+const crate = [
     ['C', 'Z', 'N', 'B', 'M', 'W', 'Q', 'V'],
     ['H', 'Z', 'R', 'W', 'C', 'B'],
     ['F', 'Q', 'R', 'J'],
@@ -28,5 +28,34 @@ fs.readFile('Day05/input2', 'utf8', (err, data) => {
         temp = parsedDatum.split(" ");
         newParsedData.push([parseInt(temp[1]), parseInt(temp[3]), parseInt(temp[5])]);
     })
-    console.log(newParsedData)
+
+    // let crate1 = [...crate];
+    // newParsedData.forEach((newParsedDatum)=> {
+    //     for (let i=0; i< newParsedDatum[0]; i++) {
+    //         crate1[newParsedDatum[2] - 1].push(crate1[newParsedDatum[1]-1].pop())
+    //     }
+    // })
+
+    // let answer = [];
+    // crate1.forEach((val) => {
+    //     answer.push(val[val.length-1])
+    // })
+    // console.log(answer) // QNHWJVJZW
+
+    // part 2
+    let crate2 = crate;
+    newParsedData.forEach((newParsedDatum) => {
+        const movingCrate= []
+        for (let i = 0; i < newParsedDatum[0]; i++) {
+            movingCrate.push(crate2[newParsedDatum[1] - 1].pop())
+        }
+        crate2[newParsedDatum[2] - 1]= crate2[newParsedDatum[2] - 1].concat(movingCrate.reverse())
+    })
+
+    let answer2 = [];
+    crate2.forEach((val) => {
+        answer2.push(val[val.length - 1])
+    })
+    console.log(answer2) // BPCZJLFJW
+
 })
