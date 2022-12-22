b5a450d84d7a77868ab44f38f886559dadebb74f
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-12-22 01:03:38 -0800

    dir

---
+++ Day07/day7.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+fs.readFile('Day07/input', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    let dir = {}
+    console.log(data);
+
+})
