e5a3505f815f75663a626c2545536284148f3f3e
Yu-HuanWu <yuhuanhwu@gmail.com>
2023-01-04 08:29:35 -0800

    parsing data

--- Day08/day8.js
+++ Day08/day8.js
@@ -5,5 +5,14 @@ fs.readFile('Day08/input', 'utf8', (err, data) => {
         console.error(err);
         return;
     }
-
+    
+    parsedData = [];
+    data.split("\n").forEach((datum) => {
+        let temp = [];
+        datum.split("").forEach((num) => {
+            temp.push(parseInt(num))
+        })
+        parsedData.push(temp)
+    })
+    console.log(parsedData)
 })
