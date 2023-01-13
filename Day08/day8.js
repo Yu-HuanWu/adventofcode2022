29d9db2c4563da6253c87a9ac66b5b8d370b29f8
Yu-HuanWu <yuhuanhwu@gmail.com>
2023-01-13 09:18:07 -0800

    comment

--- Day08/day8.js
+++ Day08/day8.js
@@ -28,6 +28,7 @@ fs.readFile('Day08/input', 'utf8', (err, data) => {
 
     for (let i = 0; i < parsedData.length; i ++) {
         for (let j = 0; j < parsedData[i].length; j++) {
+            // check for the the whole row and column, all the way to edges
             checkSurrounding(parsedData[i][j], parsedData[i][j-1], parsedData[i][j+1], parsedData[i-1][j], parsedData[i+1][j])
         }
     }
